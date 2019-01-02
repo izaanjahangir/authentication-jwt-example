2635a94b13562c5525ea903d0214640ca61fdc75
izaanjahangir <izaanjahangir2@gmail.com>
2019-01-02 19:33:30 +0500

    changed database name

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // connection URI
-const mongoURI = "mongodb://localhost/ali-restaurant";
+const mongoURI = "mongodb://localhost/authentication-jwt";
 
 // remove deprecation warning of collection.ensureIndex
 mongoose.set('useCreateIndex', true);
